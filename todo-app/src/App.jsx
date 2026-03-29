7216a57b877d88be273c985c138e3101c9c02a1f
Hasan Coder 66 <hasanashraf874@gmail.com>
2026-03-29 20:42:41 +0500

    todo app finally done

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,30 +1,68 @@
 import React, { useState } from 'react'
-import TodoItem from './components/TodoItem'
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const App = () => {
-  const [input, setInput] = useState("")
-  const [todos, setTodos] = useState([])
+ const [input, setInput] = useState("")
+ const [todos, setTodos] = useState([])
+ const [editTodo, setEditTodo] = useState(null)
 
-  const addTodo = () => {
+ const addTodo = () => {
 
-    if(input.trim() == "") return
+  if(!input.trim()) return toastHandler("Please enter a value!")
 
-    const newTodo = {
-      text: input,
-      id: Date.now(),
+  if(todos){
+    let findItemInTodo = todos.find((item) => item.title.includes(input))
+
+    if(findItemInTodo) {
+      setInput("")
+      toastHandler("Todo Already Exist!")
+      return
     }
+  }
+  
+  // if(editTodo){
+  //   const updateTodo = todos.map((todo) => todo.id == editTodo ? {...todo, title:input}: todo)
+  //   console.log(updateTodo)
 
-    setTodos([...todos, newTodo])
+  //   setTodos(updateTodo)
+  //   setEditTodo(null)
+  //   setInput("")
+  //   return
+  // }
+
+  if(editTodo){
+    const updateTodos = todos.map((todo) => todo.id == editTodo ? {...todo, title: input}: todo)
+
+    setTodos(updateTodos)
+    setEditTodo(null)
     setInput("")
+    return
   }
+  
+  const todoItem = {
+    title : input,
+    id : Date.now()
+  }
+  setTodos((prev) => [...prev, todoItem])
+   setInput("")
+ }
+  
+ const toastHandler = (message) => {
+  toast(message)
+ }
 
-  const deleteTodo = (id) => {
-console.log("delete todo chalaa",id)
-let returnFilter = todos.filter((todo) => todo.id !== id)
-setTodos(returnFilter)
-console.log(returnFilter)
-  }
-  // console.log(todos)
+ const deleteTodo = (id) => {
+  let filteredItems = todos.filter((todoItem) => todoItem.id !== id)
+  setTodos(filteredItems)
+ }
+
+ const editTodoHandler = (id) => {
+  const editTodoItemFind = todos.find((todo) => todo.id == id)
+  setInput(editTodoItemFind.title)
+  setEditTodo(editTodoItemFind.id)
+ }
+
   return (
       <div className="min-h-screen bg-gray-100 dark:bg-[#0f172a] flex items-center justify-center p-4">
       
@@ -38,16 +76,16 @@ console.log(returnFilter)
         {/* Input Section */}
         <div className="flex gap-2 mb-4">
           <input
+          value={input}
             type="text"
             placeholder="Add a new task..."
             className="flex-1 px-4 py-2 rounded-xl border border-gray-300 
                        focus:outline-none focus:ring-2 focus:ring-indigo-500
                        dark:bg-[#0f172a] dark:border-gray-700 dark:text-white"
-                       value={input}
                        onChange={(e) => setInput(e.target.value)}
           />
           <button onClick={addTodo} className="px-4 py-2 bg-indigo-600 text-white rounded-xl hover:bg-indigo-700 transition">
-            Add
+            {editTodo ? "Update" : "Add"}
           </button>
         </div>
 
@@ -61,19 +99,25 @@ console.log(returnFilter)
             <div className="flex items-center gap-3">
               <input type="checkbox" className="w-4 h-4" />
               <span className="text-gray-700 dark:text-gray-200">
-                {todo?.text}
+                {todo?.title}
               </span>
             </div>
-            <button onClick={() => deleteTodo(todo.id)} className="text-red-500 hover:text-red-600 text-sm">
+            <div className='gap-5 flex'>
+              <button onClick={() => editTodoHandler(todo.id)} className="text-red-500 hover:text-red-600 text-sm cursor-pointer">
+              Edit
+            </button>
+            <button onClick={() => deleteTodo(todo.id)} className="text-red-500 cursor-pointer hover:text-red-600 text-sm">
               Delete
             </button>
+            </div>
           </div>
-        )): "No Items In Todo"}
+        )): <p className="text-gray-500 text-center">No todos yet 🚀</p>}
         
 
         </div>
 
       </div>
+      <ToastContainer />
     </div>
   )
 }
